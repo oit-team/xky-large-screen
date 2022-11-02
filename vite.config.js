6aa59addf0341c972a655623c6520c24873046f8
邱旭 <uxuip@outlook.com>
2022-11-02 18:13:43 +0800

    feat: 安装eslint插件，更新eslint规则

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
+import Eslint from 'vite-plugin-eslint'
 
 const config = defineConfig({
   resolve: {
@@ -36,6 +37,7 @@ const config = defineConfig({
       ],
       dts: 'src/auto-imports.d.ts',
     }),
+    Eslint(),
   ],
 
   server: {
